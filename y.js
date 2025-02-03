09836f01720dc34eda589e8eb48c347f67c3c26d
Aisbir Never Sad <aisbirkun@gmail.com>
2025-02-04 01:03:34 +0700

    Delete y.js

--- y.js
+++
@@ -1,7 +0,0 @@
-
-const { createPaymentQr } = require('.')
-async function main(){
-const p = await createPaymentQr("aisbirpedia", { amount: 10000, message: "semangat bang" })
-console.log(p)
-}
-main()
